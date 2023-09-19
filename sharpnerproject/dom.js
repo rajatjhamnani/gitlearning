b0ebde7cce14d2d9a6112f956a9c296e48e280a3
rajatjhamnani <rajatjhamnani@gmail.com>
2023-09-19 23:51:22 +0530

    added new list

--- sharpnerproject/dom.js
+++ sharpnerproject/dom.js
@@ -25,3 +25,6 @@ items[2].style.backgroundColor="green"
 for(let i=0 ; i<items.length ; i++){
     items[i].style.fontWeight="bold"
 }
+var li =document.getElementsByTagName("li")
+
+li[0].style.backgroundColor="green"
