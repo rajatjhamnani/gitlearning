e620b84be379ae1c2fb67ec850a784908c4c8d0c
rajatjhamnani <rajatjhamnani@gmail.com>
2023-09-21 01:03:09 +0530

    js dom manipulations

--- sharpnerproject/dom.js
+++ sharpnerproject/dom.js
@@ -44,8 +44,82 @@ titles[0].textContent='hello'
 var odd=document.querySelectorAll('li:nth-child(odd)')
 var even=document.querySelectorAll('li:nth-child(even)')
 
-for(let i=0 ; i<=odd.length ; i++){
-    
-    odd[i].style.backgroundColor="#f4f4f4"
-    even[i].style.backgroundColor="#ccc"
-}
+var itemlist=document.querySelector("#items")
+console.log(itemlist.parentNode)
+itemlist.parentNode.style.backgroundColor="skyblue"
+itemlist.parentNode.parentNode.style.backgroundColor="blue"
+
+parentelement=document.querySelector('.list-group-item')
+console.log(parentelement.parentElement)
+
+console.log(itemlist.children)
+itemlist.children[2].style.backgroundColor="pink"
+itemlist.firstElementChild.textContent="I Am First child element"
+//itemlist.firstChild.textContent="blank space"  //firstchild count spaces also
+
+//itemlist.lastChild.textContent="blank space"  // count spaces also
+itemlist.lastElementChild.style.backgroundColor="orange"
+itemlist.lastElementChild.textContent="i am last child element"
+
+//nextsibling
+console.log(itemlist.nextSibling)
+//nextelementsibling
+console.log(itemlist.nextElementSibling)
+
+//previoussibling
+console.log(itemlist.previousSibling)
+//previouselementsibling
+console.log(itemlist.previousElementSibling)
+itemlist.previousElementSibling.style.color="yellow"
+
+//createElement
+//create newdiv
+var newdiv=document.createElement('div')
+
+//createclass
+newdiv.className="hello"
+
+//createid
+newdiv.id="newid"
+
+//add attribute
+newdiv.setAttribute('title','hellodiv')
+
+//create text node
+var newdivtext=document.createTextNode("hello world")
+
+//add text to div
+newdiv.appendChild(newdivtext)
+
+//insert thingd in Dom
+var container=document.querySelector('header .container')
+var h1=document.querySelector('header h1')
+container.insertBefore(newdiv , h1)
+newdiv.style.fontSize="30px"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
