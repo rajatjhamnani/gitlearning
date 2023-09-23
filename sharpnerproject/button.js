f8713fe04a0802a7707f837f091e5faa54295916
rajatjhamnani <rajatjhamnani@gmail.com>
2023-09-23 23:51:38 +0530

    added filtering modifications

--- sharpnerproject/button.js
+++ sharpnerproject/button.js
@@ -1,15 +1,21 @@
 var form=document.getElementById('addForm')
 var itemlist=document.getElementById('items')
+var filter =document.getElementById('filter')
+
+
 
 form.addEventListener('submit' , additem)
 itemlist.addEventListener('click', removeitem)
+filter.addEventListener('keyup',filteritems)
 
 function additem(e){
     e.preventDefault()
    var newitem=document.getElementById('item').value;
+   var newitem1=document.getElementById('it').value
    var li=document.createElement('li');
    li.className='=list-group-item';
-   li.appendChild(document.createTextNode(newitem))
+   li.appendChild(document.createTextNode(newitem+" "+newitem1))
+   //li.appendChild(document.createTextNode(" "+newitem1))
    itemlist.appendChild(li)
 //creating edit button 
 var editbutton=document.createElement("button")
@@ -32,4 +38,33 @@ function removeitem(e){
             itemlist.removeChild(li)
         }
     }
+}
+
+
+function filteritems(e){
+    var text=e.target.value.toLowerCase()
+   var items=itemlist.getElementsByTagName('li')
+   Array.from(items).forEach(function(item ){
+    var itemname=item.firstChild.textContent;
+    if(itemname.toLowerCase().indexOf(text)!= -1){
+       item.style.display='block'
+    }else{
+        item.style.display='none'
+    }
+    
+   })
+}
+
+function filteritems(e){
+    var text=e.target.value.toLowerCase()
+   var items=itemlist.getElementsByTagName('li')
+   Array.from(items).forEach(function(item ){
+    var itemname=item.firstChild.textContent;
+    if(itemname.toLowerCase().indexOf(text)!= -1){
+       item.style.display='block'
+    }else{
+        item.style.display='none'
+    }
+    
+   })
 }
